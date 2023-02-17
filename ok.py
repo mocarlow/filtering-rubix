3b8e6ab03c6828dd12bb92dea3df531a3652a293
MoCarlow <124927526+mocarlow@users.noreply.github.com>
2023-02-17 10:40:22 +0330

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,28 @@
+import os
+from os import system
+system("pip install socket")
+system("pip install sys")
+system("pip install _thread")
+system("pip install colorama")
+system("cd code-filter")
+system("clear")
+import colorama
+import time
+from colorama import Fore
+from time import sleep
+system("clear")
+Hello="""
+    Hello!
+        I am mocarlow!
+            I,am programmer
+                Me Hacker Web And Ip system!
+                    me coder Bat net!
+                    -------------######mocarlow######------------
+
+
+"""
+for i in Hello:
+    sleep(0.005)
+    print(Fore.GREEN+i,end='',flush=True)
+time.sleep(5)
+system("python start.py")
